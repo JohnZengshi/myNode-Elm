8e3f637c9ac7ce96343fbb8c2a5ca6cd5b99ab20
heidanchaofanhei <woaiwojia8664>
2018-04-25 18:28:37 +0800

    增加注册路由

--- routes/admin.js
+++ routes/admin.js
@@ -4,5 +4,6 @@ import express from 'express'
 import Admin from '../controller/admin/admin'
 const router = express.Router();
 router.post('/login', Admin.login);
+router.post('/register',Admin.register);
 
 export default router
