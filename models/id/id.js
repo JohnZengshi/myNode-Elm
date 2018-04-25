13108288199ac090fbf10bbbb006c025336e3e21
JOHNPC\JohnZeng <1351009316@qq.com>
2018-04-25 22:07:40 +0800

    配置数据id完成

--- models/id/id.js
+++ models/id/id.js
@@ -1,1 +1,19 @@
 'use strict'
+import mongoose from 'mongoose';
+let Schema = mongoose.Schema;
+
+let idSchema = new Schema({
+    user_id: Number
+})
+
+let idModel = mongoose.model("testId", idSchema);
+idModel.findOne((error, data) => {
+    if (!data) {
+        let newid = new idModel({
+            user_id: 0
+        })
+        newid.save();
+    }
+})
+
+export default idModel;
