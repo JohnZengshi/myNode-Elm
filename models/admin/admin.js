29a3e53edad7e632e3f14a093df5328a50705315
heidanchaofanhei <woaiwojia8664>
2018-04-25 18:27:41 +0800

    用户信息模板

--- models/admin/admin.js
+++ models/admin/admin.js
@@ -1,3 +1,17 @@
 'use strict';
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
+
+const AdminSchema = new Schema({
+    user_name: String,
+    password: String,
+    id: Number,
+    create_time: String,
+    status: Number,
+    city: String,
+    avatar: String,
+    admin: String,
+})
+const AdminModel = mongoose.model('admin', AdminSchema);
+
+export default AdminModel;
